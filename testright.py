e019ba782c2e2077cb0a3a305b3594c97797c31f
AnnaWeiss98 <43806309+AnnaWeiss98@users.noreply.github.com>
2018-10-19 12:20:53 +0300

    Update testright.py

--- testright.py
+++ testright.py
@@ -1,170 +1,57 @@
-import unicodedata #this is a module 
-"""
-tokenizer
-this modul performs tokenisation of a text and extracts tokens
-"""
-class Token(object):  # example, part of token's klass
-    """
-    Class of tokens taken from a given text
-    """
-    def __init__(self,position,s):  # constructor, self-object,others atr
-        """
-        Constructor for token.
-        @param position: position is an index of the first element of token
-        @param s: s is a presentation of token's string
-        @return: token
-        """
-        self.position=position  # token's position in the text
-        self.s=s  # write the meaning of variable
+import unittest
+import tokenisation
+from tokenisation import Tokenizer
+from collections import Generator
+class TestMyCode (unittest.TestCase):
+    # making a unit of Tokeniser class
+    def setUp(self):
+        self.x = Tokenizer()
+    # the test itself
+    '''def test_MyTokenizer(self):
+        result=self.x.tokenize(' h50 ht ? 20 h d sun)')
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].s, 'h')
+        self.assertEqual(result[0].position, 1)'''
+    def test_begins_with_no_alpha(self):
+        result=list(self.x.tokenize(' h50 ht ? 20 h d sun)'))
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].s, 'h')
+        self.assertEqual(result[0].position, 1)
+        self.assertEqual(result[1].s, 'ht')
+        self.assertEqual(result[1].position, 5)
+    def test_begins_with_alpha(self):
+        result=list(self.x.tokenize('h50 ht ? 20 h d sun)'))
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].s, 'h')
+        self.assertEqual(result[0].position, 0)
+        self.assertEqual(result[1].s, 'ht')
+        self.assertEqual(result[1].position, 4)
+    def test_ends_with_no_alpha(self):
+        result=list(self.x.tokenize(' h50 ht ? 20 h d sun)'))
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].s, 'h')
+        self.assertEqual(result[0].position, 1)
+        self.assertEqual(result[4].s, 'sun')
+        self.assertEqual(result[4].position, 17)
+    def test_ends_with_alpha(self):
+        result=list(self.x.tokenize(' h50 ht ? 20 h d sun'))
+        self.assertIsInstance(result, list)
+        self.assertEqual(len(result), 5)
+        self.assertEqual(result[0].s, 'h')
+        self.assertEqual(result[0].position, 1)
+        self.assertEqual(result[4].s, 'sun')
+        self.assertEqual(result[4].position, 17)
+    def test_MyError_number(self):
+        with self.assertRaises(ValueError):
+            list(self.x.tokenize(12))
+    def test_MyError_notList(self):
+        s=[1, 2, 3, 'my name is Anya']
+        with self.assertRaises(ValueError):
+            list(self.x.tokenize(s))
+if __name__== '__main__':
+    unittest.main()
         
-    def __repr__(self):
-        """
-        a function that asign numbers to tokens
-        """
-        return self.s+'_'+str(self.position)
-
-class Token_1 (Token):  # example, part of token's klass
-    """
-    Class of tokens taken from a given text
-    """
-    def __init__(self,s,t):  # constructor, self-object,others atr
-        """
-        Constructor for token.
-        @param position: position is an index of the first element of token
-        @param s: s is a presentation of token's string
-        @return: token
-        """
-        self.s=s  # write the meaning of variable
-        self.t=t #type
-    def __repr__(self):
-        """
-        a function that asign numbers to tokens
-        this function returns token and its type
-        """
-        
-        return self.s+'_'+str(self.t)
-    
-   
-class Tokenizer(object):
-    
-    """
-    Class that makes a list of tokens
-    """
-    def tokenize(self,strim):
-        """
-        A function that returns tokens with alphabetic symbols
-        @param: strim of text
-        @return: a list of tokens
-        """
-        if not isinstance(strim,str):
-            raise ValueError('Input has an unappropriate type,it should be str')
-        tokensback=[]  # clean list
-        position=0
-        if strim[0].isalpha():
-            inToken = True
-        else:
-            inToken = False
-        for i,c in enumerate(strim):
-            if c.isalpha()and not strim[i-1].isalpha():
-                position=i
-                inToken=True
-            if not c.isalpha()and strim[i-1].isalpha() and inToken:
-                s=strim[position:i]  # срез
-                t=Token(position,s)  
-                tokensback.append(t)  # writing token to list
-                inToken = False
-        """
-        condition for alphabetic symbol standing without nonalphabetic symbol after it
-        it is important for the end of text
-        """
-        if c.isalpha():
-            s=strim[position:i+1]     
-            t=Token(position,s)
-            tokensback.append(t)
-        return tokensback
-        if c.isalpha():
-            s=strim[position:i-1]     
-            t=Token(position,s)
-            tokensback.append(t)
-        return tokensback
-
-    def tokenize_GENERATOR(self,strim):
-        """
-        A function that returns tokens with alphabetic symbols
-        @param: strim of text
-        @return: a list of tokens
-        """
-        if not isinstance(strim,str):
-            raise ValueError('Input has an unappropriate type,it should be str')
-        position=0
-        if strim[0].isalpha():
-            inToken = True
-        else:
-            inToken = False
-        for i,c in enumerate(strim):
-            if c.isalpha()and not strim[i-1].isalpha():
-                position=i
-                inToken=True
-            if not c.isalpha()and strim[i-1].isalpha() and inToken:
-                s=strim[position:i]  # срез
-                t=Token(position,s)  
-                yield (t)  # writing token 
-                inToken = False
-        """
-        condition for alphabetic symbol standing without nonalphabetic symbol after it
-        it is important for the end of text
-        """
-        if c.isalpha():
-            s=strim[position:i+1]     
-            t=Token(position,s)
-            yield(t)
-        if c.isalpha():
-            s=strim[position:i-1]     
-            t=Token(position,s)
-            yield(t)
-            
-    @staticmethod
-    def get_type(c):
-       """
-       this function cheks the type
-       """
-       if c.isalpha():
-           T='A'
-       if c.isdigit():
-            T='D'
-       if c.isspace():
-           T='S'
-           # we are looking for spaces
-           #(punctuation)
-       if unicodedata.category(c)[0]=='P':
-           T='P'
-       return T
-    
-    def tokenize_GENERATOR_type(self,strim):
-        """
-        A function that returns tokens with alphabetic symbols
-        @param: strim of text
-        @return: a list of tokens
-        """
-        if not isinstance(strim,str):
-            raise ValueError('Input must be str !')
-        position=0
-        for i,c in enumerate(strim):
-            if self.get_type(c) != self.get_type(strim[i-1]) and i>0:
-                T= self.get_type(strim[i-1])
-                s=strim[position:i]  # representation of string
-                position=i
-                t=Token_1(s,T)  
-                yield (t)
-        """
-        condition for alphabetic symbol standing without nonalphabetic symbol after it
-        it is important for the end of text
-        """
-        if self.get_type(c):
-            T= self.get_type(c)
-            s=strim[position:i+1]
-            t=Token_1(s,T)
-            yield(t)
-x = Tokenizer()
-for i in x.tokenize_GENERATOR_type(' h50 ht ? 20 h d sun'):
-    print(i) 
