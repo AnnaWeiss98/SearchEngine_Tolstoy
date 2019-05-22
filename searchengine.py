841b8f96476315efe226e06d5c1935dceac1dd6a
AnnaWeiss98 <43806309+AnnaWeiss98@users.noreply.github.com>
2019-05-22 11:47:18 +0300

    Update searchengine.py

--- searchengine.py
+++ searchengine.py
@@ -4,43 +4,43 @@ import os
 
 
 class TokenWindow(object):
-
-    def __init__(self, allString ,toc, start, end):
-
-        self.allString  = allString # строка в которой нашли токен полностью
-        self.token = toc #  искомый токен
-        self.win_start = start # начало окна
-        self.win_end   = end # конец окна
+    def __init__(self, allString, toc, start, end):
+        self.allString = allString  
+        self.token = toc  
+        self.win_start = start  
+        self.win_end = end  
 
     def __repr__(self):
-
-        s= '{}, {}, {}, {}'.format(self.allString, self.token, self.win_start, self.win_end)
+        s = '{}, {}, {}, {}'.format(self.allString, str(self.token), self.win_start, self.win_end)
 
         return s
-
 
     def __eq__(self, obj):
         '''
         check if two tokens are equal (it is so when they have the
         same first and last symbol
         '''
-        return self.token == obj.token and self.win_start==obj.win_start and self.win_end == obj.win_end
+        return self.token == obj.token and self.win_start == obj.win_start and self.win_end == obj.win_end
+
+    def window_is_junction(self, obj):
+        return (self.win_start <= obj.win_end and
+                self.win_end >= obj.win_start and
+                obj.allString == self.allString)
 
 
 class SearchEngine(object):
     """
     Class containing methods for working with database.
     """
+
     def __init__(self, database):
         """
         Create an instance of SearchEngine class.
         """
         self.database = shelve.open(database)
 
-
     def __del__(self):
         self.database.close()
-
 
     def search(self, query):
         """
@@ -72,14 +72,13 @@ class SearchEngine(object):
         for f in results_of_search:
             list_of_files = list_of_files & f
         # create a dictionary of positions of all query tokens in files
-        final_dict ={}
+        final_dict = {}
         for f in list_of_files:
             final_dict[f] = []
             for token in searchlist:
                 final_dict[f].extend(self.database[token][f])
             final_dict[f].sort()
         return final_dict
-
 
     def find_window(self, findstr, window_len):
 
@@ -88,46 +87,67 @@ class SearchEngine(object):
         if not findstr:
             return {}
 
-        windows = {} 
-        tokenizer = Tokenizer()  
-        result_dict = self.multiple_search(findstr)  
-        
+        windows = {}
+        tokenizer = Tokenizer()
+        result_dict = self.multiple_search(findstr)
+
         for file_key in result_dict.keys():
             wins = []
             result_list = result_dict[file_key]
 
-
             for result_token in result_list:
 
                 with open(file_key) as f:
-                   for i, line in enumerate(f):
-                       if i == result_token.string: # i - the number that we are looking for
+                    for i, line in enumerate(f):
+                        if i == result_token.string:
                             break
-                line = line.strip("\n") # remove from line the symbol of transfer
+                line = line.strip("\n")
 
                 right_context = line[result_token.start:]
-                left_context = line[:result_token.end][::-1] # overturned with a slice 
+                left_context = line[:result_token.end][::-1]
 
                 i = 0
                 for token in tokenizer.tokenize_generator_type(left_context):
-                    if token.t in ['A','D']:
-                       i+=1
+                    if token.t in ['A', 'D']:
+                        i += 1
                     if i == window_len:
-                         break
-                
+                        break
+
                 start = result_token.end - token.position - len(token.s)
 
                 i = 0
                 for token in tokenizer.tokenize_generator_type(right_context):
-                    if token.t in ['A','D']:
-                       i+=1
+                    if token.t in ['A', 'D']:
+                        i += 1
                     if i == window_len:
-                         break
+                        break
 
                 end = result_token.start + token.position + len(token.s)
-                wins.append (TokenWindow (line, result_token, start, end))
-                                  
+                wins.append(TokenWindow(line, [result_token], start, end))
+
             if len(wins) > 0:
-               windows[file_key] = wins
-        		 
-        return windows
+                windows[file_key] = wins
+
+        return self.join_windows(windows)
+
+    def join_windows(self, _dict):
+
+        window_dict = {}
+
+        for f, wins in _dict.items():
+            pr_win = None
+            for win in wins:
+                if pr_win is not None and pr_win.window_is_junction(win):
+                    for tok in win.token:
+                        if tok not in pr_win.token:
+                            pr_win.token.append(tok)
+
+                    start = min(pr_win.win_start, win.win_start)
+                    end = max(pr_win.win_end, win.win_end)
+                else:
+                    if pr_win is not None:
+                        window_dict.setdefault(f, []).append(pr_win)
+                    pr_win = win
+            window_dict.setdefault(f, []).append(pr_win)
+
+        return window_dict
