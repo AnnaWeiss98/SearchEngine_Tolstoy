468c551104dc80238b380277e5edd77c10fbca1b
AnnaWeiss98 <43806309+AnnaWeiss98@users.noreply.github.com>
2019-06-04 13:38:49 +0300

    Update tokenwindow.py

--- tokenwindow.py
+++ tokenwindow.py
@@ -9,11 +9,16 @@ def main():
         if findstr == "exit":
             break
 
-        res = win.find_supplemented_window(findstr, 2)
-        for k in res:
+        res = win.find_supplemented_window(findstr, 2)#2 - deth of window
+        for k in res: #k - key
             print(k)
             for v in res[k]:
-                print(v)
+                print(v) #v- meaning from the list
+                print (v.get_BB_string())
 
 if __name__ == "__main__":
     main()
+"""
+the window is the dictionary, where a key is a file name,
+and the meaning is a list of windows
+"""
