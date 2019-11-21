268d4132b1715e2334e342e253e0a36ea99aea96
AnnaWeiss98 <43806309+AnnaWeiss98@users.noreply.github.com>
2019-11-21 10:38:00 +0300

    Update server.py

--- server.py
+++ server.py
@@ -6,7 +6,8 @@ from searchengine import SearchEngine
 body = '''
 <!DOCTYPE html>
 <html>
-    <head>        <meta http-equiv="Content-Type" content="text/html; charset=cp1251" />
+    <head>    
+        <meta http-equiv="Content-Type" content="text/html; charset=cp1251" />
         <title>Поиск</title>
     </head>
     <body>
@@ -17,8 +18,10 @@ body = '''
      <td><input type="text" name="findstr" value={}></td>
      <td><input type="submit" value="Search"></td>
      <tr>
-     <td>First tom:</td>
-     <td><input type="text" name="offset" value="{}" size="4"></td>
+     <td><input type="submit" name="action" value="begin"></td>
+     <td><input type="submit" name="action" value="back"></td>
+     <td><input type="submit" name="action" value="forward"></td> 
+     <td><input type="hidden" name="offset" value="{}" size="4"></td>
      </tr>
      <tr>
      <td>Count tom:</td>
@@ -34,17 +37,19 @@ body = '''
 form_limit ='''
      <table>
      <tr>
-     <td colspan="2" align="center"> Для файла {} </td>
+     <td colspan="2" align="center"> Для файла {0} </td>
      </tr>
      <tr>
-     <td>first record:</td>
-     <td><input type="text" name="doc{}offset" value="{}" size="4"></td>
+     <td><input type="submit" name="action{1}" value="begin"></td>
+     <td><input type="submit" name="action{1}" value="back"></td>
+     <td><input type="submit" name="action{1}" value="forward"></td> 
+     <td><input type="hidden" name="doc{1}offset" value="{2}" size="4"></td>
      </tr>
      <tr>
      <td>a number of results on the page:</td>
-     <td><input type="text" name="doc{}limit" value="{}" size="4"></td>
+     <td><input type="text" name="doc{3}limit" value="{4}" size="4"></td>
      </table>
-     {}
+     {5}
  '''
 
 class custom_handler(BaseHTTPRequestHandler):
@@ -76,9 +81,16 @@ class custom_handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
+
+        print (postvars)
         result = self.gen_page(postvars)
-        offset = self.get_int(postvars['offset'][0],0)
+
         limit =  self.get_int(postvars['limit'][0],4)
+        offset = self.get_int(postvars['offset'][0],0)
+        if 'action' in postvars:
+            offset = self.get_offset(postvars['action'][0],offset,limit)
+
+        
         self.wfile.write(bytes(body.format(postvars['findstr'][0],str(offset),str(limit),result), 'cp1251'))
 
     def get_int(self,value, def_value):
@@ -89,41 +101,71 @@ class custom_handler(BaseHTTPRequestHandler):
             pass
         return result
 
+    def get_offset(self, act, offset,delta):
+
+        res = offset
+        if act == 'forward':
+           res += delta
+        elif act == 'back':
+           res -= delta
+        elif act == 'begin':
+           res = 0
+
+        if res < 0:
+            res = 0
+ 
+        return res
+
 
     def gen_page(self, postvars):
+
         result = ''
         findstr = postvars['findstr'][0]
         limit = self.get_int(postvars['limit'][0], 4)
-        offset = self.get_int(postvars['offset'][0], 0)
+
+        offset = self.get_int(postvars['offset'][0],0)
+        if 'action' in postvars:
+            offset = self.get_offset(postvars['action'][0],offset,limit)
 
         o = []
         l = []
         for ind in range(offset, offset+limit):
-            if 'doc'+str(ind)+'offset' in postvars:  
-                o.append(self.get_int(postvars['doc'+str(ind)+'offset'][0],0))
-                l.append(self.get_int(postvars['doc' + str(ind) + 'limit'][0],5))
+            if 'doc'+str(ind)+'offset' in postvars:
+               
+                limit_doc = self.get_int(postvars['doc' + str(ind) + 'limit'][0],5)
+                offset_doc = self.get_int(postvars['doc'+str(ind)+'offset'][0],0)
+                if 'action'+str(ind) in postvars:                
+                    offset_doc = self.get_offset(postvars['action'+str(ind)][0], offset_doc,limit_doc)
+
+                o.append(offset_doc)
+                l.append(limit_doc)
             else:
                 o.append(0)
                 l.append(5)
 
         limits = list(zip(o,l))
 
-        res = self.server.search_engine.find_supplemented_window(findstr, 2, offset, limit, limits)
+        res = self.server.search_engine.find_supplemented_window_lim(findstr, 2, offset, limit, limits)
 
-        print (res.keys())
         for i,k in enumerate(res.keys()):
-            limit = 5
-            offset = 0
+            limit_doc = 5
+            offset_doc = 0
+               
             if 'doc'+str(i+offset)+'limit' in postvars:
-                 limit = self.get_int(postvars['doc'+str(i+offset)+'limit'][0],5)
-                 offset = self.get_int(postvars['doc'+str(i+offset)+'offset'][0],0)
+
+                limit_doc = self.get_int(postvars['doc'+str(i+offset)+'limit'][0],5)
+
+                offset_doc = self.get_int(postvars['doc'+str(i+offset)+'offset'][0],0)
+
+                if 'action'+str(i+offset) in postvars:                
+                    offset_doc = self.get_offset(postvars['action'+str(i+offset)][0], offset_doc,limit_doc)
 
             re = '<ul>'
             for v in res[k]:
                 re += '<li>' + v.get_BB_string() + '</li>'
             re += '</ul>'
 
-            result += form_limit.format(k, str(i), str(offset), str(i), str(limit),re)
+            result += form_limit.format(k, str(i+offset), str(offset_doc), str(i+offset), str(limit_doc), re)
 
 
         return result
