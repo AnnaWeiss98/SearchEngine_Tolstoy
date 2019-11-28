452feeea09215d18389a3fb8df94138b8d010f33
AnnaWeiss98 <43806309+AnnaWeiss98@users.noreply.github.com>
2019-11-28 09:47:39 +0300

    Update server.py

--- server.py
+++ server.py
@@ -15,19 +15,21 @@ body = '''
      <table>
      <tr>
      <td>Searchword:</td>
-     <td><input type="text" name="findstr" value={0}>
-         <input type="hidden" name="prev_findstr" value={0}>
+     <td><input type="text" name="findstr" value="{0}">
+         <input type="hidden" name="prev_findstr" value="{0}">
      </td>
      <td><input type="submit" value="Search"></td>
      <tr>
-     <td><input type="submit" name="action" value="begin"></td>
-     <td><input type="submit" name="action" value="back"></td>
-     <td><input type="submit" name="action" value="forward"></td> 
+     <td><input type="submit" name="action" value="begin" {5} ></td>
+     <td><input type="submit" name="action" value="back" {5} ></td>
+     <td><input type="submit" name="action" value="forward" {4} ></td> 
      <td><input type="hidden" name="offset" value="{1}" size="4"></td>
      </tr>
      <tr>
      <td>Count tom:</td>
-     <td><input type="text" name="limit" value="{2}" size="4"></td>
+     <td><input type="text" name="limit" value="{2}" size="4">
+         <input type="hidden" name="limit_prev" value="{2}" size="4">
+     </td>
      </tr>
      </table>
      {3}
@@ -36,26 +38,28 @@ body = '''
 </html>
 '''
 
-form_limit ='''
+form_limit = '''
      <table>
      <tr>
      <td colspan="2" align="center"> Для файла {0} </td>
      </tr>
      <tr>
-     <td><input type="submit" name="action{1}" value="begin"></td>
-     <td><input type="submit" name="action{1}" value="back"></td>
-     <td><input type="submit" name="action{1}" value="forward"></td> 
+     <td><input type="submit" name="action{1}" value="begin" {6} ></td>
+     <td><input type="submit" name="action{1}" value="back" {6} ></td>
+     <td><input type="submit" name="action{1}" value="forward" {5} ></td> 
      <td><input type="hidden" name="doc{1}offset" value="{2}" size="4"></td>
      </tr>
      <tr>
      <td>a number of results on the page:</td>
-     <td><input type="text" name="doc{3}limit" value="{4}" size="4"></td>
+     <td><input type="text" name="doc{1}limit" value="{3}" size="4">
+         <input type="hidden" name="doc{1}limit_prev" value="{3}" size="4">
+     </td>
      </table>
-     {5}
+     {4}
  '''
 
-class custom_handler(BaseHTTPRequestHandler):
 
+class custom_handler(BaseHTTPRequestHandler):
     def do_GET(self):
         """
         Creates a simple HTML page with a field and a button.
@@ -63,7 +67,7 @@ class custom_handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
         result = ''
-        self.wfile.write(bytes(body.format('','0','4',result), 'cp1251'))
+        self.wfile.write(bytes(body.format('', '0', '4', result, '', ''), 'cp1251'))
 
     def do_POST(self):
         """
@@ -83,21 +87,10 @@ class custom_handler(BaseHTTPRequestHandler):
         self.send_response(200)
         self.end_headers()
 
-
         print (postvars)
-        result = self.gen_page(postvars)
+        self.wfile.write(bytes(self.gen_page(postvars), 'cp1251'))
 
-        limit =  self.get_int(postvars['limit'][0],4)
-        offset = self.get_int(postvars['offset'][0],0)
-        if 'action' in postvars:
-            offset = self.get_offset(postvars['action'][0],offset,limit)
-        
-        if postvars['findstr'][0] !=  postvars['prev_findstr'][0]:
-           offset = 0
-        
-        self.wfile.write(bytes(body.format(postvars['findstr'][0],str(offset),str(limit),result), 'cp1251'))
-
-    def get_int(self,value, def_value):
+    def get_int(self, value, def_value):
         result = def_value
         try:
             result = int(value)
@@ -105,86 +98,111 @@ class custom_handler(BaseHTTPRequestHandler):
             pass
         return result
 
-    def get_offset(self, act, offset,delta):
+    def get_offset(self, act, offset, delta):
 
         res = offset
         if act == 'forward':
-           res += delta
+            res += delta
         elif act == 'back':
-           res -= delta
+            res -= delta
         elif act == 'begin':
-           res = 0
+            res = 0
 
         if res < 0:
             res = 0
- 
-        return res
 
+        return res
 
     def gen_page(self, postvars):
 
-        result = ''
+        result = ''  # Result boby page
+        isNew = False  # Flag is New data for findstr or action for files or limits for files
+
         findstr = postvars['findstr'][0]
         limit = self.get_int(postvars['limit'][0], 4)
+        offset = self.get_int(postvars['offset'][0], 0)
 
-        offset = self.get_int(postvars['offset'][0],0)
+        # if  press button navigation in files parametrs
         if 'action' in postvars:
-            offset = self.get_offset(postvars['action'][0],offset,limit)
+            offset = self.get_offset(postvars['action'][0], offset, limit)
+            isNew = True
 
-        if postvars['findstr'][0] !=  postvars['prev_findstr'][0]:
-           offset = 0
+        # if  findstr is changed or limits for files canged
+        if postvars['findstr'][0] != postvars['prev_findstr'][0] or postvars['limit'][0] != postvars['limit_prev'][0]:
+            offset = 0
+            isNew = True
 
         o = []
         l = []
-        for ind in range(offset, offset+limit):
-            if 'doc'+str(ind)+'offset' in postvars:
-               
-                limit_doc = self.get_int(postvars['doc' + str(ind) + 'limit'][0],5)
-                offset_doc = self.get_int(postvars['doc'+str(ind)+'offset'][0],0)
-                if 'action'+str(ind) in postvars:                
-                    offset_doc = self.get_offset(postvars['action'+str(ind)][0], offset_doc,limit_doc)
+        for ind in range(0, limit + 1):
+            if 'doc' + str(ind) + 'offset' in postvars:
 
-                if postvars['findstr'][0] !=  postvars['prev_findstr'][0]:
-                      offset_doc = 0
+                limit_doc = self.get_int(postvars['doc' + str(ind) + 'limit'][0], 5)
+                offset_doc = self.get_int(postvars['doc' + str(ind) + 'offset'][0], 0)
+
+                # if press button navigation in quote
+                if 'action' + str(ind) in postvars:
+                    offset_doc = self.get_offset(postvars['action' + str(ind)][0], offset_doc, limit_doc)
+
+                # if changed limits in currend quotes
+                if postvars['doc' + str(ind) + 'limit_prev'][0] != postvars['doc' + str(ind) + 'limit_prev'][0]:
+                    offset_doc = 0
+
+                if isNew:
+                    offset_doc = 0
+                    limit_doc = 5
 
                 o.append(offset_doc)
-                l.append(limit_doc)
+                l.append(limit_doc + 1)
             else:
                 o.append(0)
-                l.append(5)
+                l.append(6)
 
-        limits = list(zip(o,l))
+        limits = list(zip(o, l))
 
-        res = self.server.search_engine.find_supplemented_window_lim(findstr, 2, offset, limit, limits)
+        res = self.server.search_engine.find_supplemented_window_lim(findstr, 5, offset, limit + 1, limits)
 
-        for i,k in enumerate(res.keys()):
-            limit_doc = 5
-            offset_doc = 0
-               
-            if 'doc'+str(i+offset)+'limit' in postvars:
+        disabled_f = ''
+        if limit >= len(res.keys()):
+            disabled_f = 'disabled'
 
-                limit_doc = self.get_int(postvars['doc'+str(i+offset)+'limit'][0],5)
+        disabled_b = ''
+        if offset == 0:
+            disabled_b = 'disabled'
 
-                offset_doc = self.get_int(postvars['doc'+str(i+offset)+'offset'][0],0)
+        for i, k in enumerate(res.keys()):
 
-                if 'action'+str(i+offset) in postvars:                
-                    offset_doc = self.get_offset(postvars['action'+str(i+offset)][0], offset_doc,limit_doc)
+            if i == limit:
+                break
 
-                if postvars['findstr'][0] !=  postvars['prev_findstr'][0]:
-                      offset_doc = 0
+            limit_doc = limits[i][1] - 1
+            offset_doc = limits[i][0]
+
             re = '<ul>'
-            for v in res[k]:
+
+            disabled_f_doc = ''
+            if limit_doc >= len(res[k]):
+                disabled_f_doc = 'disabled'
+
+            disabled_b_doc = ''
+            if offset_doc == 0:
+                disabled_b_doc = 'disabled'
+
+            for j, v in enumerate(res[k]):
+                if j == limit_doc:
+                    break
                 re += '<li>' + v.get_BB_string() + '</li>'
+
             re += '</ul>'
 
-            result += form_limit.format(k, str(i+offset), str(offset_doc), str(i+offset), str(limit_doc), re)
+            result += form_limit.format(k, str(i), str(offset_doc), str(limit_doc), re, disabled_f_doc, disabled_b_doc)
 
+        ret_result = body.format(postvars['findstr'][0], str(offset), str(limit), result, disabled_f, disabled_b)
 
-        return result
+        return ret_result
 
 
 if __name__ == '__main__':
-
     httpd = HTTPServer(('localhost', 80), custom_handler)
     httpd.search_engine = SearchEngine('database')
     print ('Start server.')
